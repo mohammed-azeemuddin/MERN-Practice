cca68e0450c3cff66915bca1f25f98df37b5fe79
mohammed-azeemuddin <mohammedazeemuddin98@gmail.com>
2022-01-23 22:21:44 +0530

    modified server.js

--- Web Development/my-express-server/server.js
+++ Web Development/my-express-server/server.js
@@ -4,6 +4,23 @@ const express = require('express');
 
 const app = express();
 
+app.get("/",function(req, res){
+  // console.log(request);
+  res.send("<h1>hello</h1>");
+});
+
+app.get("/contact", function(req,res){
+  res.send("Contact me at : abc@gmail.com");
+});
+
+app.get("/about", function(req,res){
+  res.send("I am Azeem. Express check");
+});
+
+app.get("/hobbies", function(req,res){
+  res.send("I love playing cricket");
+});
+
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
