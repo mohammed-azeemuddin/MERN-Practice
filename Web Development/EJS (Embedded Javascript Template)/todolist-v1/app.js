dcf343f0c91fa2238cfd8b9e7a0418c89946231a
mohammed-azeemuddin <mohammedazeemuddin98@gmail.com>
2022-02-07 19:21:55 +0530

    added new files

--- Web Development/EJS (Embedded Javascript Template)/todolist-v1/app.js
+++ Web Development/EJS (Embedded Javascript Template)/todolist-v1/app.js
@@ -3,44 +3,37 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+let items = ["Buy Food","Cook Food","Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res) {
 
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
+  let today = new Date();
 
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-    console.log("Error! current day is equal to "+ currentDay);
-  }
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("en-US",options);
 
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItems: items
   });
 
+});
+
+app.post("/", function(req,res) {
+  let item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 
