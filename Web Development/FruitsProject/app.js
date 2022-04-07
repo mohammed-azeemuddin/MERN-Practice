4250ab188134ed03d167ebac46dfc505e9433360
mohammed-azeemuddin <mohammedazeemuddin98@gmail.com>
2022-04-08 01:03:26 +0530

    added app.js

---
+++ Web Development/FruitsProject/app.js
@@ -0,0 +1,44 @@
+//jshint esversion:6
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'fruitsDB';
+
+// Create a new MongoClient
+const client = new MongoClient(url, {useUnifiedTopology: true});
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  insertDocuments(db, function() {
+      client.close();
+  });
+});
+
+const insertDocuments = function(db, callback) {
+  // Get the documents collection
+  const collection = db.collection('fruits');
+  // Insert some documents
+  collection.insertMany([
+
+     {name : "Apple", score: 8, review: "Great fruit"}, {name : "Orange", score: 6, review: "Kinda sour"}, {name : "Banana", score: 9, review: "Great stuff!"}
+
+  ], function(err, result) {
+
+    assert.equal(err, null);
+    assert.equal(3,result.insertedCount);
+    assert.equal(3,Object.keys(result.insertedIds).length);
+
+    console.log("Inserted 3 documents into the collection");
+
+    callback(result);
+
+  });
+
+}
